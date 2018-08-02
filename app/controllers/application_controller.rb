ee3a916254d8203f23e024718439392fc3cff008
Shea Kusiak <shkusiak@gmail.com>
2018-08-02 02:25:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,7 @@ class ApplicationController < Sinatra::Base
     # deletes one recipe based on id in url
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
-    erb :index
+    redirect to '/recipes'
   end
 
 end
