d77e34e5dd315d8f85b9d9eab82a2391fb429467
Shea Kusiak <shkusiak@gmail.com>
2018-08-02 01:06:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/' do
     erb :index
   end
 
-  get '/recipe/new' do
+  get '/recipes/new' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     @recipe.save
     redirect '/'
