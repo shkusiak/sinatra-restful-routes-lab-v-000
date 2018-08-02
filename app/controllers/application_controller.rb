812efe956ef8d5f23c7c36da8a4ca0a464d5229b
Shea Kusiak <shkusiak@gmail.com>
2018-08-02 01:50:41 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id' do
     # displays one recipe based on id in url
-    @recipe = Recipe.find_by_id(params[id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
