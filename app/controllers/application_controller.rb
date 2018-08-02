821d10562a35d13ec7ded0399234c5d84ec46d75
Shea Kusiak <shkusiak@gmail.com>
2018-08-02 01:32:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   post '/recipes' do
