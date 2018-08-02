f5d252d3ada9742c2f050cc8d23c003b3362d738
Shea Kusiak <shkusiak@gmail.com>
2018-08-02 01:19:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
     # displays create recipe form
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     @recipe.save
-    redirect '/'
+    erb :index
   end
 
   get '/recipes' do
